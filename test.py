2cf858484329cbd323e81ec9ed2772fb4ce09d70
Zeinab Daliri <zedal8353@oslomet.no>
2026-01-19 15:42:52 +0100

    Add first Python file

---
+++ test.py
@@ -0,0 +1,20 @@
+
+
+x = "Hi"
+y= "There"
+print(x + y)
+
+print("Hei! Python 3.13 kjører i VS Code 🎉")
+print("hello world")
+
+name = input("Hva heter du? ")
+age = int(input("Hvor gammel er du? "))
+
+year_born = 2026 - age
+
+print("Du ble født i " + str(year_born))
+
+
+
+
+
